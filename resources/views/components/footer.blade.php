6651f104008faeb98aebc3de03afafd7bf6c7588
Fabian Ternis <151365993+Michaelninder@users.noreply.github.com>
2025-04-12 13:39:29 +0200

    Create footer.blade.php

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="bg-dark text-white text-center py-4">
+    <p>2025 - {{ env('APP_NAME') }}. All rights reserved.</p>
+    <ul class="list-unstyled">
+      <li><a class="text-white" href="{{ route('legal.imprint') }}">Imprint</a></li>
+      <li><a class="text-white" href="{{ route('legal.privacy') }}">Privacy Policy</a></li>
+      <li><a class="text-white" href="{{ route('legal.terms') }}">Terms of Service</a></li>
+    </ul>
+</footer>
+  
