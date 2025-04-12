5023715d210f2ff546bf5795f2e1307c0583690a
Fabian Ternis <151365993+Michaelninder@users.noreply.github.com>
2025-04-12 13:38:02 +0200

    Create main.blade.php

---
+++ resources/views/layouts./main.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>{{ env('APP_NAME') }} @yield('title')</title>
+    
+    <!-- Favicon -->
+    <!--link rel="icon" href="{{ asset('favicon.ico') }}" type="image/x-icon"-->
+    
+    <!-- Bootstrap CSS (Latest version) -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
+
+    <!-- Bootstrap Icons -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+
+    <link rel="stylesheet" href="{{ asset('css/auth.css') }}">
+</head>
+<body>
+    @include('components.navbar')
+    @yield('content')
+    @include('components.footer')
+
+    <script src="{{ asset('js/auth.js') }}"></script>
+
+    <!-- Bootstrap JS (Bundle includes both Bootstrap JS and Popper.js) -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.bundle.min.js" integrity="sha384-k6d4wzSIapyDyv1kpU366/PK5hCdSbCRGRCMv+eplOQJWyd1fbcAu9OCUj5zNLiq" crossorigin="anonymous"></script>
+</body>
+</html>
